88d2233a8ddcb8480d54dfe13d40d58b259d0b49
CMcKane <chrisrmckane@gmail.com>
2015-12-01 22:07:21 -0500

    Update Driver.java

--- src/radiogaga/Driver.java
+++ src/radiogaga/Driver.java
@@ -6,6 +6,7 @@ public class Driver
 	public static void main(String[] args)
 	{
 		generateOldSongs();
+		generateReports();
 		Playlist p = new Playlist();
 		p.readSongsFromFile("Hotlist.txt");
 		new RadioGUI_2(p);
@@ -13,41 +14,52 @@ public class Driver
 	
 	public static void generateOldSongs()
 	{
-		OldSong oldSongOne = new OldSong("Bohemian Rhapsody", "Rock", 5:55, "Queen", "A Night at the Opera", 10/31/1975);
-		OldSong oldSongTwo = new OldSong("Stairway to Heaven", "Rock", 8:03, "Led Zeppelin", "Led Zeppelin IV", 12/05/1970);
-		OldSong oldSongThree = new OldSong("Imagine", "Soft Rock", 3:03, "John Lennon", "Imagine", 10/11/1971);
-		OldSong oldSongFour = new OldSong("Smells Like Teen Spirit", "Alternative Rock", 5:01, "Nirvana", "Nevermind", 09/10/1991);
-		OldSong oldSongFive = new OldSong("Hotel California", "Soft Rock", 6:30, "Eagles", "Hotel California", 02/01/1977);
-		OldSong oldSongSix = new OldSong("Sweet Child O'Mine", "Hard Rock", 5:55, "Guns N' Roses", "Appetite for Destruction", 08/15/1988);
-		OldSong oldSongSeven = new OldSong("One", "Thrash Metal", 7:27, "Metallica", "...And Justice for All", 01/10/1989);
-		OldSong oldSongEight = new OldSong("It's Not Unusual","Pop", 2:03, "Tom Jones", "Along Came Jones", 11/11/1964);
-		OldSong oldSongNine = new OldSong("Like a Rolling Stone", "Rock", 6:13, "Bob Dylan", "Highway 61 Revisited", 07/20/1965);
-		OldSong oldSongTen = new OldSong("Hey Jude", "Rock", 7:11, "The Beatles", "Single", 08/26/1968);
+		OldSong oldSongOne = new OldSong("Bohemian Rhapsody", "Rock", 355, "Queen", "A Night at the Opera", 10/31/1975);
+		OldSong oldSongTwo = new OldSong("Stairway to Heaven", "Rock", 483, "Led Zeppelin", "Led Zeppelin IV", 12/05/1970);
+		OldSong oldSongThree = new OldSong("Imagine", "Soft Rock", 183, "John Lennon", "Imagine", 10/11/1971);
+		OldSong oldSongFour = new OldSong("Smells Like Teen Spirit", "Alternative Rock", 301, "Nirvana", "Nevermind", 09/10/1991);
+		OldSong oldSongFive = new OldSong("Hotel California", "Soft Rock", 390, "Eagles", "Hotel California", 02/01/1977);
+		OldSong oldSongSix = new OldSong("Sweet Child O'Mine", "Hard Rock", 355, "Guns N' Roses", "Appetite for Destruction", 08/15/1988);
+		OldSong oldSongSeven = new OldSong("One", "Thrash Metal", 447, "Metallica", "...And Justice for All", 01/10/1989);
+		OldSong oldSongEight = new OldSong("It's Not Unusual","Pop", 123, "Tom Jones", "Along Came Jones", 11/11/1964);
+		OldSong oldSongNine = new OldSong("Like a Rolling Stone", "Rock", 373, "Bob Dylan", "Highway 61 Revisited", 07/20/1965);
+		OldSong oldSongTen = new OldSong("Hey Jude", "Rock", 431, "The Beatles", "Single", 08/26/1968);
 	}
 	
 	public static void generateCommercials()
 	{
-		
+		Commercial comOne = new Commercial("Bose Audio", "Better Sound Through Research", 30, "Bose");
+		Commercial comTwo = new Commercial("Axe Body Spray", "'Nice' women become 'Naughty'", 30, "Axe");
+		Commercial comThree = new Commercial("Captain Morgan", "To Life, Love and Loot." , 30, "Diageo");
+		Commercial comFour = new Commercial("Samsung Galaxy S6 Edge", "Next is Now", 30, "Samsung");
+		Commercial comFive = new Commercial("iPhone 6S", "The only thing that's changed is everything.", 30, "Apple");
+		Commercial comSix = new Commercial("Xbox One", "All for one. Input one.", 30, "Microsoft");
+		Commercial comSeven = new Commercial("Playstation 4", "Where the Greatest Play", 30, "Sony Computer Entertainment");
+		Commercial comEight = new Commercial("Fallout 4", "Welcome Home", 30, "Bethesda Game Studios");
+		Commercial comNine = new Commercial("Kia Optima", "The Power to Surprise", 30, "Kia Motors");
+		Commercial comTen = new Commercial("Colegate Toothpaste", "Improve mouth health simply by brushing.", 30, "Colgate");
+		Commercial comEleven = new Commercial();
+		Commercial comTwelve = new Commercial();
+		Commercial comThirteen = new Commercial();
+		Commercial comeFourteen = new Commercial();
+		Commercial comFifteen = new Commercial();
 	}
 	
-	public static void generateNews()
+	public static void generateReports()
 	{
+		//News Report
+		Report newsReport = new Report("News Report", "Update about the local news.", 60, NEWS);
 		
-	}
-	
-	public static void generateWeather()
-	{
+		//Weather Report
+		Report weatherReport = new Report("Weather Report", "Update about the local weather.", 30, WEATHER);
 		
-	}
-	
-	public static void generateTraffic()
-	{
-		
+		//Traffic Report
+		Report trafficeReport = new Report("Traffic Report", "Update about the current local traffic.", 60, TRAFFIC);
 	}
 	
 	public static void generateRadioSpot()
 	{
-		
+		Spot radioSpot = new Spot("")
 	}
 
 }
