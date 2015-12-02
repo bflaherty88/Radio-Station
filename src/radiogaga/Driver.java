d5c1214da50e67cdc3543d9b5e13621b315d830e
CMcKane <chrisrmckane@gmail.com>
2015-12-01 20:59:07 -0500

    Update Driver.java

--- src/radiogaga/Driver.java
+++ src/radiogaga/Driver.java
@@ -9,5 +9,19 @@ public class Driver
 		p.readSongsFromFile("Hotlist.txt");
 		new RadioGUI_2(p);
 	}
+	
+	public static void generateOldSongs()
+	{
+		oldSongOne = new OldSong();
+		oldSongTwo = new OldSong();
+		oldSongThree = new OldSong();
+		oldSongFour = new OldSong();
+		oldSongFive = new OldSong();
+		oldSongSix = new OldSong();
+		oldSongSeven = new OldSong();
+		oldSongEight = new OldSong();
+		oldSongNine = new OldSong();
+		oldSongTen = new OldSong();
+	}
 
 }
